39e2452ba942f72ebb5adfa56638b45e41099ec8
Dalton Carr <daltonlcarr@yahoo.com>
2017-12-14 18:17:30 -0800

    fixing model for pre and post functions

--- model/wizard.js
+++ model/wizard.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const mongoose = require('mongoose');
+const Category = require('./category');
+const httpErrors = require('http-errors');
 
 const wizardSchema = mongoose.Schema ({
   name:{
@@ -21,7 +23,40 @@ const wizardSchema = mongoose.Schema ({
     type : mongoose.Schema.Types.ObjectId,
     required : true,
     ref : 'magic',
-  }
+  },
 });
+
+//-----------------------------------------------------
+// SETTING UP RELATIONSHIP MANAGEMENT
+//-----------------------------------------------------
+wizardSchema.pre('save',function(done){
+  //making sure that the category exists before saving a note
+  return Category.findById(this.category)
+    .then(categoryFound => {
+      if(!categoryFound)
+        throw httpErrors(404,'category not found');
+      
+      categoryFound.wizards.push(this._id);
+      return categoryFound.save();
+    })
+    .then(() => done())
+    .catch(done);// this will trigger an error
+});
+
+wizardSchema.post('remove',(document,done) => {
+  return Category.findById(document.category)
+    .then(categoryFound => {
+      if(!categoryFound)
+        throw httpErrors(404,'category not found');
+      
+      categoryFound.wizards = categoryFound.notes.filter(wizard => {
+        return wizard._id.toString() !== document._id.toString();
+      });
+      return categoryFound.save();
+    })
+    .then(() => done())
+    .catch(done);
+});
+//-----------------------------------------------------
 
 module.exports = mongoose.model('wizard',wizardSchema);
